bb54347763c884e3416f01d06a29761f72d66774
hyeonho choi <hyunho1232@naver.com>
2021-01-12 18:09:16 +0900

    Revert "프레임워크 1차 수정"
    This reverts commit 55641dfd991c3c33d0dca955186fdbe744ccf713.

--- Team6_UMB/Form1.Designer.cs
+++ Team6_UMB/Form1.Designer.cs
@@ -29,17 +29,10 @@ namespace Team6_UMB
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1384, 861);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
