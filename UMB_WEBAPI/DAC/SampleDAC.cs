e7760de5c4be7133892fe8eb4904a64fdb954bed
승준 진 <wnsghkdlxld@naver.com>
2021-01-15 13:30:51 +0900

    WebAPI SampleDAC (DB연결)

---
+++ UMB_WEBAPI/DAC/SampleDAC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace UMB_WEBAPI.DAC
+{
+    public class SampleDAC
+    {
+        SqlConnection conn;
+        public SampleDAC()
+        {
+            conn = new SqlConnection(Connection.strConn);
+            conn.Open();
+        }
+    }
+}
