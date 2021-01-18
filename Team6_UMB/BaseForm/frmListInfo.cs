2be9e84961d46308c961bdef06ff19b59608f155
hyeonho choi <hyunho1232@naver.com>
2021-01-18 18:19:27 +0900

    메인폼 네비바 삭제

---
+++ Team6_UMB/BaseForm/frmListInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team6_UMB.BaseForm
+{
+    public partial class frmListInfo : Form
+    {
+        public frmListInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
