5356075b0161d4a905bc56ca55da15b21cbfeed8
hyeonho choi <hyunho1232@naver.com>
2021-01-27 21:55:30 +0900

    품목관리 1/3 최현호 210127

--- Team6_UMB/Forms/CHH/frmProductManage.cs
+++ Team6_UMB/Forms/CHH/frmProductManage.cs
@@ -5,35 +5,93 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Team6_UMB.Service;
+using UMB_VO.CHH;
 
 namespace Team6_UMB.Forms
 {
     public partial class frmProductManage : Team6_UMB.frmBaseList
     {
-        string headerName;
+        ProdStatusService service;
+        List<ProdStatusVO> allList;
+        int price_present, product_lorder_count, product_safety_count, company_id, w_id;
+        string product_id, product_name, product_type, product_unit, w_name, company_name, product_exam, price_yn, product_stnd, product_comment, product_deleted;
+
+        
+
         public frmProductManage()
         {
             InitializeComponent();
-            
-        }
-
-        private void btnAllButtons1_btnCreate_Event(object sender, EventArgs e)
-        {
-            headerName = "품목관리 - 등록";
-            frmProductManagePopUp frm = new frmProductManagePopUp(headerName);
-            frm.ShowDialog();
-        }
-
-        private void btnAllButtons1_btnUpdate_Event(object sender, EventArgs e)
-        {
-            headerName = "품목관리 - 수정";
-            frmProductManagePopUp frm = new frmProductManagePopUp(headerName);
-            frm.ShowDialog();
+            newBtns.btnShipment.Visible = newBtns.btnWait.Visible = newBtns.btnSearch.Visible = newBtns.btnDocument.Visible = false;
         }
 
         private void frmProductManage_Load(object sender, EventArgs e)
         {
+            CommonUtil.SetInitGridView(dgvProduct);
+            CommonUtil.AddGridTextColumn(dgvProduct, "번호", "product_id", 60);                   //0
+            CommonUtil.AddGridTextColumn(dgvProduct, "제품명", "product_name", 150);              //1
+            CommonUtil.AddGridTextColumn(dgvProduct, "제품분류", "product_type", 100);            //2
+            CommonUtil.AddGridTextColumn(dgvProduct, "단위", "product_unit", 100);                //3
+            CommonUtil.AddGridTextColumn(dgvProduct, "현재단가", "price_present", 100);           //4
+            CommonUtil.AddGridTextColumn(dgvProduct, "창고명", "w_name", 150);                    //5
+            CommonUtil.AddGridTextColumn(dgvProduct, "최소발주량", "product_lorder_count", 100);  //6
+            CommonUtil.AddGridTextColumn(dgvProduct, "안전재고량", "product_safety_count", 100);  //7
+            CommonUtil.AddGridTextColumn(dgvProduct, "업체명", "company_name", 170);              //8
+            CommonUtil.AddGridTextColumn(dgvProduct, "검사여부", "product_exam", 90);             //9
+            CommonUtil.AddGridTextColumn(dgvProduct, "사용유무", "price_yn", 90);                 //10
+            CommonUtil.AddGridTextColumn(dgvProduct, "규격", "product_stnd", 130);               //11
+            CommonUtil.AddGridTextColumn(dgvProduct, "비고", "product_comment", 10, false);      //12
+            CommonUtil.AddGridTextColumn(dgvProduct, "삭제여부", "product_deleted", 90);          //13
+            CommonUtil.AddGridTextColumn(dgvProduct, "거래처id", "company_id", 10, false);        //14
+            CommonUtil.AddGridTextColumn(dgvProduct, "창고id", "w_id", 10, false);                //15
 
+
+            DGV_Binding();
+        }
+
+        private void DGV_Binding()
+        {
+            service = new ProdStatusService();
+            allList = service.GetProdInfo();
+            dgvProduct.DataSource = allList;
+        }
+
+        private void newBtns_btnRefresh_Event(object sender, EventArgs e)
+        {
+            DGV_Binding();
+        }
+
+        private void newBtns_btnCreate_Event(object sender, EventArgs e)
+        {
+            string headerName = "품목 등록";
+            frmProductManagePopUp frm = new frmProductManagePopUp(headerName);
+            frm.Show();
+        }
+
+        private void newBtns_btnUpdate_Event(object sender, EventArgs e)
+        {
+            string headerName = "품목 수정";
+            frmProductManagePopUp frm = new frmProductManagePopUp(headerName, product_id, product_name, product_type, product_unit, price_present, w_name, product_lorder_count, product_safety_count, company_name, product_exam, price_yn, company_id, w_id, product_stnd, product_comment, product_deleted);
+            frm.Show();
+        }
+        private void dgvProduct_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            product_id = dgvProduct.Rows[e.RowIndex].Cells[0].Value.ToString();
+            product_name = dgvProduct.Rows[e.RowIndex].Cells[1].Value.ToString();
+            product_type = dgvProduct.Rows[e.RowIndex].Cells[2].Value.ToString();
+            product_unit = dgvProduct.Rows[e.RowIndex].Cells[3].Value.ToString();
+            price_present = int.Parse(dgvProduct.Rows[e.RowIndex].Cells[4].Value.ToString());
+            w_name = dgvProduct.Rows[e.RowIndex].Cells[5].Value.ToString();
+            product_lorder_count = int.Parse(dgvProduct.Rows[e.RowIndex].Cells[6].Value.ToString());
+            product_safety_count = int.Parse(dgvProduct.Rows[e.RowIndex].Cells[7].Value.ToString());
+            company_name = dgvProduct.Rows[e.RowIndex].Cells[8].Value.ToString();
+            product_exam = dgvProduct.Rows[e.RowIndex].Cells[9].Value.ToString();
+            price_yn = dgvProduct.Rows[e.RowIndex].Cells[10].Value.ToString();
+            product_stnd = dgvProduct.Rows[e.RowIndex].Cells[11].Value.ToString();
+            product_comment = dgvProduct.Rows[e.RowIndex].Cells[12].Value.ToString();
+            product_deleted = dgvProduct.Rows[e.RowIndex].Cells[13].Value.ToString();
+            company_id = int.Parse(dgvProduct.Rows[e.RowIndex].Cells[14].Value.ToString());
+            w_id = int.Parse(dgvProduct.Rows[e.RowIndex].Cells[15].Value.ToString());
         }
     }
 }
--- Team6_UMB/Forms/CHH/frmProductManagePopUp.cs
+++ Team6_UMB/Forms/CHH/frmProductManagePopUp.cs
@@ -14,17 +14,12 @@ namespace Team6_UMB.Forms
         {
             InitializeComponent();
             this.label1.Text = headerName;
+        }
 
-            if (headerName == "품목관리 - 등록")
-            {
-                btnCreate.Dock = DockStyle.Fill;
-                btnUpdate.Visible = false;
-            }
-            else if (headerName == "품목관리 - 수정")
-            {
-                btnUpdate.Dock = DockStyle.Fill;
-                btnCreate.Visible = false;
-            }
+        public frmProductManagePopUp(string headerName, string product_id, string product_name, string product_type, string product_unit, int price_present, string w_name, int product_lorder_count, int product_safety_count, string company_name, string product_exam, string price_yn, int company_id, int w_id, string product_stnd, string product_comment, string product_deleted)
+        {
+            InitializeComponent();
+            this.label1.Text = headerName;
         }
 
         private void button3_Click(object sender, EventArgs e)
